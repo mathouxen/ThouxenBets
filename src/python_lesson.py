6f18bd8bbc405a957458c5ccd9bbc2d4190f283b
kamarjhb025 <kamarjhb025@student.wethinkcode.co.za>
2026-09-25 09:17:42 +0200

    let's see

---
+++ src/python_lesson.py
@@ -0,0 +1,25 @@
+# functions in python
+
+def adding(num1, num2):
+    result = num1 + num2
+    if result % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+
+
+
+
+
+
+
+
+
+
+sum = 1 + 4
+
+if sum % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
